0b4612c6f0ec7e5109952b602fad5561ae36b30d
Zachary Zhou <yixinzhou728817@gmail.com>
2026-04-08 11:11:01 -0700

    update the requirements.txt and I test my firt api

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,16 +1,20 @@
+#这个文件要做两件事情分别是加载config配置并且注册health_bp这个Blueprint#
+
 from flask import Flask
+from flask_cors import CORS
+from app.config import Config #import config class, flask can read the config of Supabase#
+from app.blueprints.health import health_bp
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config) #import the config class to flask app#
 
-    print("🔥 create_app 已执行")
+    CORS(app)
 
-    @app.route("/")
-    def home():
-        return "HOME OK"
+    app.register_blueprint(health_bp) # register the health_bp into the falsk, then the /api/health will be activated#
 
-    @app.route("/test")
-    def test():
-        return "TEST OK"
+    print('successfullt implemented health blueprints')
 
     return app
+
+    #CORS -> Cross-Origin Resource Sharing, 允许前端访问后端接口#
